7d2d1f03193911afd94b6576e197427972f93dfb
Zeke Sikelianos <zeke@sikelianos.com>
2023-05-17 18:20:30 +0000

    moar WIP

--- predict.py
+++ predict.py
@@ -12,12 +12,12 @@ from collections import OrderedDict
 from cog import BasePredictor, ConcatenateIterator, Input, Path
 
 # from config import DEFAULT_MODEL_NAME, DEFAULT_CONFIG_PATH, load_tokenizer, load_tensorizer
-from subclass import YieldingMPT
+from subclass import YieldingReplitCode
 
 # Weights are either local or in a cloud bucket.
 
 # For development, point to a local path on disk.
-# This is the path from which we pull weights when there's no COG_WEIGHTS environment variable
+# This is the path from which we pull weights when there's no COG_WEIGHTS environment variable (COG_WEIGHTS is a thing for trainable models)
 TENSORIZER_WEIGHTS_PATH = "model/replit-code-v1-3b.tensors"
 
 # Set this to a GCP URL when pushing the model
@@ -37,61 +37,16 @@ def maybe_download(path):
 
 
 class Predictor(BasePredictor):
-    def setup(self, weights: Optional[Path] = None):
+    def setup(self):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
         # set TOKENIZERS_PARALLELISM to false to avoid a warning
         os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-        if weights is not None and weights.name == "weights":
-            # bugfix
-            weights = None
-        if weights is None and TENSORIZER_WEIGHTS_PATH:
-            self.model = self.load_tensorizer(
-                weights=maybe_download(TENSORIZER_WEIGHTS_PATH), plaid_mode=True, cls=YieldingMPT, config_path=DEFAULT_CONFIG_PATH,
-            )
-        
-        elif hasattr(weights, "filename") and "tensors" in weights.filename:
-            self.model = self.load_tensorizer(
-                weights=weights, plaid_mode=True, cls=YieldingMPT, config_path=DEFAULT_CONFIG_PATH,
-            )
-        elif hasattr(weights, "suffix") and "tensors" in weights.suffix:
-            self.model = self.load_tensorizer(
-                weights=weights, plaid_mode=True, cls=YieldingMPT
-            )
-        # elif "tensors" in weights:
-        #     self.model = self.load_tensorizer(
-        #         weights=weights, plaid_mode=True, cls=YieldingMPT
-        #     )
-        else:
-            weights = "./model/"
-
-            self.model = self.load_huggingface_model(weights=weights)
-
-        self.tokenizer = self.load_tokenizer(TOKENIZER_PATH)
-    
-    def load_tokenizer(self, path):
-        tokenizer = AutoTokenizer.from_pretrained(path)
-
-        return tokenizer
-
-    def load_huggingface_model(self, weights=None):
-
-        config = AutoConfig.from_pretrained(
-            weights,
-            trust_remote_code=True
+        self.model = self.load_tensorizer(
+            weights=maybe_download(TENSORIZER_WEIGHTS_PATH), plaid_mode=True, cls=YieldingReplitCode, config_path=DEFAULT_CONFIG_PATH,
         )
-
-        config.attn_config['attn_impl'] = 'triton'
-
-        st = time.time()
-        print(f"loading weights from {weights} w/o tensorizer")
-        model = YieldingMPT.from_pretrained(
-            weights, torch_dtype=torch.bfloat16, trust_remote_code=True
-        )
-        model.to(self.device)
-        print(f"weights loaded in {time.time() - st}")
-        return model
+        self.tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_PATH)
     
     def load_tensorizer(self, weights, plaid_mode, cls, config_path):
         st = time.time()
@@ -108,16 +63,19 @@ class Predictor(BasePredictor):
         )
 
 
-        des = TensorDeserializer(weights, plaid_mode=True)
-        des.load_into_module(model)
-        model = model.to(dtype=torch.bfloat16)
+        deserialized = TensorDeserializer(weights, plaid_mode=True)
+        deserialized.load_into_module(model)
+        try:
+          model = model.to(dtype=torch.bfloat16)
+        except:
+            pass
 
         print(f"weights loaded in {time.time() - st}")
         return model
 
     def predict(
         self,
-        prompt: str = Input(description=f"Prompt to send to MPT-StoryWriter."),
+        prompt: str = Input(description=f"Text prompt"),
         max_length: int = Input(
             description="Maximum number of tokens to generate. A word is generally 2-3 tokens",
             ge=1,
